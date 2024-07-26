f7776ddf8ba808c8c929a820b1a94cab870cbdbe
Amish2417 <anishraj2417>
2024-07-26 17:37:55 +0530

    Practice-Set-27

---
+++ index.js
@@ -0,0 +1,61 @@
+
+// 1
+
+// let num =17;
+// let i = 1;
+// while(i<=10){
+//   console.log(`17 * ${i} = ${17*i}`);
+//   i++;
+// }
+
+// 2
+
+// let num = 100;
+// let i = 1;
+// let sum = 0;
+// while(i<=num){
+//   if(i%2!= 0){
+//     //console.log(i);
+//     sum += i;
+//   }
+//   i++;
+// }
+// console.log(sum);
+
+// 3
+
+// let num = 10;
+// let result = 1;
+// let i = 1;
+// while(i<=num){
+//   result *= i
+//   i++;
+// }
+// console.log(result);
+
+// 4
+
+// let num = 123456;
+// let sum = 0;
+// while(num > 0){
+//   let rem = num%10;
+//   sum += rem;
+//   num = Math.floor(num/10);
+//   console.log(num);
+  
+// }
+// console.log(sum);
+
+// 5
+
+// let num1 = 10,num2=12;
+// let num = num2;
+// let i = 1;
+// while(num % num1 != 0){
+//   i++;
+//    num = num2 * i;
+// }
+// console.log(num);
+
+
+
